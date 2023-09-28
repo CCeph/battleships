c63895dfc4e0d5ee5114751500a51266f4108692
CCeph <yousefelsisyy@gmail.com>
2023-09-28 13:39:46 -0300

    Ship objects can count hits to know if they sunk

--- src/dataHandler.js
+++ src/dataHandler.js
@@ -1,3 +1,15 @@
 export default function createShip(length) {
-  return { length };
+  const totalHits = 0;
+
+  function hit() {
+    this.totalHits += 1;
+  }
+
+  function isSunk() {
+    if (this.totalHits === this.length) {
+      return true;
+    }
+    return false;
+  }
+  return { length, totalHits, hit, isSunk };
 }
--- src/dataHandler.test.js
+++ src/dataHandler.test.js
@@ -3,3 +3,21 @@ import createShip from "./dataHandler";
 test("Ship factory creates an object with correct length provided", () => {
   expect(createShip(4).length).toBe(4);
 });
+
+test("Ship factory creates objects with a default hit count of 0", () => {
+  expect(createShip(4).totalHits).toBe(0);
+});
+
+test("Ships have a hit method to increase their totalHits", () => {
+  const ship = createShip(2);
+  expect(ship.totalHits).toBe(0);
+  ship.hit();
+  expect(ship.totalHits).toBe(1);
+});
+
+test("Ship factory creates objects that can tell if they've been sunk or not", () => {
+  const ship = createShip(1);
+  expect(ship.isSunk()).toBeFalsy();
+  ship.hit();
+  expect(ship.isSunk()).toBeTruthy();
+});
