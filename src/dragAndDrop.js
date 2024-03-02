87de26f5f763c31503b729f4b48641e5eff58d53
cceph <96626515+CCeph@users.noreply.github.com>
2024-03-02 15:00:40 -0400

    Get ship details when dragging over player cell

--- src/dragAndDrop.js
+++ src/dragAndDrop.js
@@ -21,3 +21,87 @@ cachedDOM.$draggables.forEach((draggable) => {
     draggable.classList.remove("dragging");
   });
 });
+
+/* function checkValidShipPlacement(shipInput) {
+  const shipArray = Object.entries(shipInput);
+  let mockShipboard = [
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null, null, null],
+  ];
+  const valid = shipArray.every((ship) => {
+    const shipName = ship[0];
+    const shipPlacement = ship[1];
+    const shipLength = getShipLengthByName(shipName);
+
+    const validBounds = checkValidBounds(shipPlacement, shipLength);
+    const validNoOverlap = checkNoOverlap(
+      shipPlacement,
+      shipLength,
+      mockShipboard
+    );
+    mockShipboard = validNoOverlap.mockShipboardCopy;
+    if (validBounds === true && validNoOverlap.valid === true) {
+      return true;
+    }
+    return false;
+  });
+  return valid;
+} */
+
+function getDraggingShip() {
+  const draggingElement = document.querySelector(".dragging");
+  const ship = {
+    name: draggingElement.dataset.ship,
+  };
+  if ([...draggingElement.classList].includes("vertical")) {
+    ship.orientation = "vertical";
+  } else {
+    ship.orientation = "horizontal";
+  }
+
+  switch (ship.name) {
+    case "carrier":
+      ship.length = 5;
+      break;
+
+    case "battleship":
+      ship.length = 4;
+      break;
+
+    case "cruiser":
+      ship.length = 3;
+      break;
+
+    case "destroyer":
+      ship.length = 2;
+      break;
+
+    case "submarine":
+      ship.length = 2;
+      break;
+
+    default:
+      break;
+  }
+
+  return ship;
+}
+
+function checkValidShipPlacement(ship, cell) {
+  console.log(ship, cell);
+}
+
+cachedDOM.$playerCellsList.forEach((cell) => {
+  cell.addEventListener("dragover", () => {
+    const ship = getDraggingShip();
+    // checkValidShipPlacement(ship, cell);
+  });
+});
